df72adf605ba6657c7f405a1ed91489181a8c453
ugo <ugo.zahm@gmail.com>
2021-04-14 18:04:20 +0200

    corrected var name

--- solver.py
+++ solver.py
@@ -12,23 +12,23 @@ class Solver:
 
     def build_relaxed_graph_plan(self):
         """Builds the relaxed graph plan from initial state"""
-        rgp, statements_layer = GraphManager(), {tuple(atom.predicate) for atom in self.dp.initialstate()}
-        rgp.add_layer(statements_layer, layer_params={"type": "statements"})
+        rgp, total_statements = GraphManager(), {tuple(atom.predicate) for atom in self.dp.initialstate()}
+        rgp.add_layer(total_statements, layer_params={"type": "statements"})
         while True:
-            possible_actions = self.operators_manager.get_applicable_actions(statements_layer) \
-                + [self.operators_manager.void_action(statements_layer)]
+            possible_actions = self.operators_manager.get_applicable_actions(total_statements) \
+                + [self.operators_manager.void_action(total_statements)]
             new_statements = fct.reduce(
                 lambda s1, s2: s1 | s2, [action["effect_pos"] for action in possible_actions], set()
             )
-            if statements_layer.issuperset(new_statements):
+            if total_statements.issuperset(new_statements):
                 # Breaks when no additional statement is obtained
                 break
-            statements_layer = statements_layer | new_statements
+            total_statements = total_statements | new_statements
 
             rgp.add_layer(possible_actions,
                           connection_func=lambda statement, action: statement in action["precond_pos"],
                           layer_params={"type": "actions"})
-            rgp.add_layer(statements_layer,
+            rgp.add_layer(total_statements,
                           connection_func=lambda action, statement: statement in action["effect_pos"],
                           layer_params={"type": "statements"})
         return rgp
